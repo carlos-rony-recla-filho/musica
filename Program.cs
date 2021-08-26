423b3f18714f6c8c3306648b011f9a6108d2b6ef
Carlos Rony <carlosrecla03@gmail.com>
2021-08-26 15:30:17 -0300

    finalda tarefa

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace musica
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("365 Chances (part. Sync 3)");
+            Console.WriteLine("Ao Cubo");
+            Console.WriteLine();
+            Console.ForegroundColor = ConsoleColor.DarkMagenta;
+            Console.WriteLine("Quem me dera ao menos uma vez");
+            Console.WriteLine("O simples fosse visto como o mais importante");
+            Console.WriteLine();
+             Console.WriteLine("Como entender que somos filhos do amor");
+             Console.WriteLine("Amando e sendo amado como Ele nos amou");
+             Console.WriteLine("De pobre se fez pra eu ser rico como sou");
+             Console.WriteLine("Não tenho demais e nem tão pouco faltou");
+
+             Console.ForegroundColor = ConsoleColor.Green;
+             
+             Console.WriteLine();
+             Console.WriteLine("refrão");
+             Console.WriteLine();
+
+             Console.ForegroundColor = ConsoleColor.DarkMagenta;
+             Console.WriteLine("Trezentos e sessenta e cinco chances");
+             Console.WriteLine("Trezentos e sessenta e cinco chances");
+             Console.WriteLine();
+              Console.WriteLine("O ano inteiro pra ser alguém melhor, cara");
+              Console.WriteLine("Amar o próximo, sara");
+              Console.WriteLine("Respeito é a chave, chapa");
+
+
+
+
+             Console.ResetColor();
+        }
+    }
+}
